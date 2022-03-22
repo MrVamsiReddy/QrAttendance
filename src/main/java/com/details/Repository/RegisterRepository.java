e471489d777ecaa9313f62d21f4f071ac50bb226
MrVamsiReddy <mrvamsi552@gmail.com>
2022-03-21 17:56:57 -1200

    Revert "first commit"
    This reverts commit 4f12974d16151a7bd8541e07e6368f2ebb30c3c5.

--- src/main/java/com/details/Repository/RegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.details.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.details.Entity.attendance;
-
-public interface RegisterRepository extends JpaRepository<attendance,Long> {
-
-}
